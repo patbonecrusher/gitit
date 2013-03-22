4c229aba8653b61f5aca345603c9b7b87c9a68bd
Pat Laplante <pat@covenofchaos.com>
2013-03-22 14:09:43 -0400

    passing storing local config test

--- Gemfile
+++ Gemfile
@@ -3,12 +3,11 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in gitit.gemspec
 gemspec
 
-gem "ruby", :require => "optparse", 
-            :require => "base64", 
-            :require => "logger", 
-            :require => "singleton"
+gem "ruby", :require => "optparse", :require => "logger", :require => "singleton"
 
+gem "kruptos", :require => "kruptos/blowfish"
 gem "rspec"
 gem "rspec-core"
 gem "simplecov"
+gem "active_support", :require => "base64"
     
--- spec/gitit_spec.rb
+++ spec/gitit_spec.rb
@@ -2,7 +2,7 @@ require "gitit"
 
 module Gitit
 
-  describe Gitit do
+  describe Git do
 
     # -------------------------------------------------------------------------
     # -------------------------------------------------------------------------
@@ -16,20 +16,20 @@ module Gitit
       end
 
       it "will open fine given a valid repo" do
-        lambda{GitIt.new(Dir.pwd)}.should_not raise_error
+        lambda{Git.new(Dir.pwd)}.should_not raise_error
       end
 
       it "will throw an error given an invalid repo" do
-        lambda{GitIt.new(BAD_PATH)}.should raise_error
+        lambda{Git.new(BAD_PATH)}.should raise_error
       end
 
       it "will return true on valid repo" do
-        myRepo = GitIt.new(Dir.pwd)
+        myRepo = Git.new(Dir.pwd)
         myRepo.isAValidRepo?.should eq true 
       end
 
       it "will return false on invalid repo" do
-        myRepo = GitIt.new(TMP_PATH)
+        myRepo = Git.new(TMP_PATH)
         myRepo.isAValidRepo?.should eq false
       end
 
@@ -41,7 +41,7 @@ module Gitit
 
       before(:each) do
         FileUtils.mkpath TEST_REPO_PATH
-        @myRepo = GitIt.new(TEST_REPO_PATH)
+        @myRepo = Git.new(TEST_REPO_PATH)
       end
     
       it "will initialize a new repo on an existing folder" do
@@ -61,6 +61,61 @@ module Gitit
       end
 
     end
+
+    # -------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
+    describe "#configItem" do
+
+      KEY_NAME = "mytest.bla"
+      KEY_VALUE = "osd aas as dsaadk".force_encoding("UTF-8")
+
+      before(:each) do
+        FileUtils.mkpath TEST_REPO_PATH
+        @myRepo = Git.new(TEST_REPO_PATH)
+        @myRepo.createNewRepo
+      end
+    
+      describe "while not using encryption" do
+        it "will set the specified value successfully" do
+          lambda{@myRepo.setConfigValue(KEY_NAME, KEY_VALUE, false)}.should_not raise_error
+        end
+        
+        it "will retrieve the specified value successfully" do
+          value = ""
+          lambda{@myRepo.setConfigValue(KEY_NAME, KEY_VALUE, false)}.should_not raise_error
+          lambda{value = @myRepo.getConfigValue(KEY_NAME, false)}.should_not raise_error
+          value.should eq KEY_VALUE
+        end
+      end
+
+      describe "while using encryption" do
+        it "will encrypt and set the specified value successfully" do
+          lambda{@myRepo.setConfigValue(KEY_NAME, KEY_VALUE, true)}.should_not raise_error
+        end
+
+        it "will store an encrypted value in the config" do
+          lambda{@myRepo.setConfigValue(KEY_NAME, KEY_VALUE, true)}.should_not raise_error
+
+          value = ""
+          lambda{value = @myRepo.getConfigValue(KEY_NAME, false)}.should_not raise_error
+          value.should_not eq KEY_VALUE
+        end
+        
+        it "will retrieve and decrypt the specified value from the config" do
+          lambda{@myRepo.setConfigValue(KEY_NAME, KEY_VALUE, true)}.should_not raise_error
+
+          value = ""
+          lambda{value = @myRepo.getConfigValue(KEY_NAME, true)}.should_not raise_error
+          value.should eq KEY_VALUE
+        end
+      end
+      
+      after(:each) do
+        FileUtils.rm_rf TEST_REPO_PATH
+      end
+
+    end
+
 
   end
     
